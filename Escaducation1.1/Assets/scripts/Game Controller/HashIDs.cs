70715e66ecf7ebcccda8a6da4f245419e583c655
hRienmueller <hannah.rienmueller@stud.h-da.de>
2015-11-05 11:46:36 +0100

    try01 for character controller

---
+++ Escaducation1.1/Assets/scripts/Game Controller/HashIDs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashIDs : MonoBehaviour {
+
+    public int speedFloat;
+    public int sneakingBool;
+
+
+    public int playersInSightBool;
+	
+
+    void Awake()
+    {
+        speedFloat = Animator.StringToHash("Speed");
+        sneakingBool = Animator.StringToHash("Sneaking");
+
+    }
+}
---
+++ Escaducation1.1/Assets/scripts/Player/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float turnSmoothing = 15f;
+    public float speedDampTime = 0.1f;
+    public GameObject Player;
+
+    private Animator anim; //reference to the player animator
+    private HashIDs hash;  //reference to the asIDs script
+    private Rigidbody rigidbody;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        hash = GameObject.FindGameObjectWithTag("gameController").GetComponent<HashIDs>();
+        rigidbody = Player.GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool sneak = Input.GetButton("Sneak");
+
+        MovementManagement(h, v, sneak);
+    }
+
+
+    void MovementManagement(float horizontal, float vertical, bool sneaking)
+    {
+        anim.SetBool(hash.sneakingBool, sneaking);
+
+        if(horizontal != 0f || vertical != 0f)
+        {
+            Rotating(horizontal, vertical);
+            anim.SetFloat(hash.speedFloat, 5.5f, speedDampTime, Time.deltaTime);
+        }
+        else
+        {
+            anim.SetFloat(hash.speedFloat, 0f);
+        }
+    }
+
+
+    void Rotating(float horizontal, float vertical)
+    {
+        Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+        rigidbody.MoveRotation(newRotation);
+    }
+
+
+    
+
+}
