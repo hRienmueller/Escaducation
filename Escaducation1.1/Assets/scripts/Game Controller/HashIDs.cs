a7a752fe5da925d0b9da1f1ca9ad35a473c80651
hRienmueller <hannah.rienmueller@stud.h-da.de>
2015-11-09 10:35:30 +0100

    PlayerController implemented

--- Escaducation1.1/Assets/scripts/Game Controller/HashIDs.cs
+++ Escaducation1.1/Assets/scripts/Game Controller/HashIDs.cs
@@ -1,19 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HashIDs : MonoBehaviour {
-
+public class HashIDs : MonoBehaviour
+{
     public int speedFloat;
     public int sneakingBool;
-
-
-    public int playersInSightBool;
-	
-
+    //public int playerInSightBool;
+    
     void Awake()
     {
         speedFloat = Animator.StringToHash("Speed");
         sneakingBool = Animator.StringToHash("Sneaking");
-
+        //playerInSightBool = Animator.StringToHash("PlayerInSight");
     }
 }
--- Escaducation1.1/Assets/scripts/Player/PlayerMovement.cs
+++ Escaducation1.1/Assets/scripts/Player/PlayerMovement.cs
@@ -1,24 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerMovement : MonoBehaviour
+{
     public float turnSmoothing = 15f;
     public float speedDampTime = 0.1f;
-    public GameObject Player;
 
-    private Animator anim; //reference to the player animator
-    private HashIDs hash;  //reference to the asIDs script
+    private Animator anim;
+    private HashIDs hash;
     private Rigidbody rigidbody;
-
 
     void Awake()
     {
         anim = GetComponent<Animator>();
-        hash = GameObject.FindGameObjectWithTag("gameController").GetComponent<HashIDs>();
-        rigidbody = Player.GetComponent<Rigidbody>();
-    }
+        hash = GameObject.FindGameObjectWithTag("gameController").GetComponent<HashIDs>() ;
 
+        rigidbody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
@@ -29,32 +27,26 @@ public class PlayerMovement : MonoBehaviour {
         MovementManagement(h, v, sneak);
     }
 
-
     void MovementManagement(float horizontal, float vertical, bool sneaking)
     {
         anim.SetBool(hash.sneakingBool, sneaking);
 
-        if(horizontal != 0f || vertical != 0f)
+        if (horizontal != 0f || vertical != 0f)
         {
             Rotating(horizontal, vertical);
-            anim.SetFloat(hash.speedFloat, 5.5f, speedDampTime, Time.deltaTime);
+            anim.SetFloat(hash.speedFloat, 1f, speedDampTime, Time.deltaTime);
         }
         else
         {
-            anim.SetFloat(hash.speedFloat, 0f);
+            anim.SetFloat(hash.speedFloat, 0);
         }
     }
-
 
     void Rotating(float horizontal, float vertical)
     {
         Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
         Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
-        Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+        Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing*Time.deltaTime);
         rigidbody.MoveRotation(newRotation);
     }
-
-
-    
-
 }
