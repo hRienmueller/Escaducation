0adcc90be7853d756ed6af3d499cccc50cb7143d
hRienmueller <hannah.rienmueller@stud.h-da.de>
2015-11-23 14:07:56 +0100

    score boost on toilets implemented

---
+++ Escaducation1.1/Assets/scripts/Game Controller/ToiletScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToiletScript : MonoBehaviour
+{
+    public bool GetScoreBoost;
+    public int scoreBoost;
+
+    private PlayerScore playerScore;
+    private Score scoreScript;
+
+    void Start()
+    {
+        playerScore = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScore>();
+        scoreScript = GameObject.FindGameObjectWithTag("gameController").GetComponent<Score>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (GetScoreBoost == false)
+        {
+            playerScore.score = playerScore.score + scoreBoost;
+            PlayerPrefs.SetInt("IntScore", playerScore.score);
+            PlayerPrefs.Save();
+            GetScoreBoost = true;
+        }
+    }
+}
