dfaaaa6c7daf0403c4e04a24ddcc9635e80e79be
hRienmueller <hannah.rienmueller@stud.h-da.de>
2015-11-30 12:31:25 +0100

    time Limit per Level implemented

---
+++ Escaducation1.1/Assets/TimeBarScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeBarScript : MonoBehaviour
+{
+    public float numTime = 3600;
+    Image TimeBar;
+
+    private onButtonClick Endscript;
+
+    void Start()
+    {
+        Endscript = GameObject.FindGameObjectWithTag("gameController").GetComponent<onButtonClick>();
+        GameObject TimeSlider = GameObject.FindGameObjectWithTag("TimeBar");
+        TimeBar = TimeSlider.GetComponent<Image>();
+        TimeBar.fillAmount = 0f;
+        
+    }
+
+    void FixedUpdate()
+    {
+        float fillPerFrame = 1 / numTime;
+
+        TimeBar.fillAmount = TimeBar.fillAmount + fillPerFrame;
+        if (TimeBar.fillAmount == 1)
+        {
+            Endscript.changeScenes("StartScreen");
+        }
+    }
+}
