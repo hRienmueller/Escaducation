ff5cf3c82b33465a2a7e0b3977bae674b45ae63f
hRienmueller <hannah.rienmueller@stud.h-da.de>
2015-12-01 16:02:23 +0100

    -roof for enterable(is that even a word?) rooms implemented, that disappears, if the player is inside the room -All three Levels first Version

---
+++ Escaducation1.1/Assets/scripts/roofScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class roofScript : MonoBehaviour
+{
+    public GameObject Roof;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Roof.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exited.");
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player Exited");
+            Roof.SetActive(true);
+        }
+    }
+}
